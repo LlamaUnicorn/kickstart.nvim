3549fcfe4a086d8b66993e98d0c4d4c1d823aeda
LlamaUnicorn <holidaynight@gmail.com>
2024-07-06 11:21:01 +0300

    add colors but they still dont work

--- init.lua
+++ init.lua
@@ -738,8 +738,9 @@ require('lazy').setup({
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
+    dir = '~/.config/nvim/lua/colors/mellow.vim',
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'daylerees/colour-schemes',
+    'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
